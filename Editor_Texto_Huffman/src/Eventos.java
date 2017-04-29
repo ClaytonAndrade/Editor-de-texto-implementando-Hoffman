946e0ede0a3e88c9a335167304f237c8da5c8220
Carlos Fernandes <Carlos Fernandes@192.168.1.13>
2017-04-28 22:54:51 -0300

    Projeto atulizado faltando so a compactacao

---
+++ Editor_Texto_Huffman/src/Eventos.java
@@ -0,0 +1,176 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Carlos Fernandes
+ */
+public class Eventos implements ActionListener {
+
+    SetGet s = new SetGet();
+    TelaPrincipal telaprincipal;
+    File file2;
+
+    public Eventos(TelaPrincipal telaprincipal) {
+        this.telaprincipal = telaprincipal;
+    }
+
+    public void Abrir() throws IOException {
+        JFileChooser file = new JFileChooser();
+        file.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        FileNameExtensionFilter arquivo = new FileNameExtensionFilter("Arquivo de Texto", "txt");
+        file.setFileFilter(arquivo);
+        int retorno = file.showOpenDialog(telaprincipal);
+
+        if (retorno == JFileChooser.APPROVE_OPTION) {
+            file2 = new File(file.getSelectedFile().getAbsolutePath());
+            try {
+                FileReader fis = new FileReader(file2);
+                BufferedReader bis = new BufferedReader(fis);
+
+                String linha = bis.readLine();
+
+                while (linha != null) {
+                    telaprincipal.jTextArea.append(linha + "\n");
+                    linha = bis.readLine();
+
+                }
+                bis.close();
+                fis.close();
+            } catch (FileNotFoundException e) {
+                
+            } catch (IOException e) {
+                
+            }
+
+        }
+
+    }
+
+    public void salvarcomo() {
+        JFileChooser file = new JFileChooser();
+        file.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        FileNameExtensionFilter arquivo = new FileNameExtensionFilter("Arquivo de Texto", "txt");
+        file.setFileFilter(arquivo);
+        int retorno = file.showOpenDialog(telaprincipal);
+        try {
+            FileWriter escreve;
+            escreve = new FileWriter(file.getSelectedFile().getAbsolutePath());
+
+            escreve.write(telaprincipal.jTextArea.getText());
+            escreve.close();
+        } catch (Exception erro) {
+
+        }
+    }
+
+    public void salvar() throws IOException {
+
+        if (file2 == null) {
+            salvarcomo();
+            file2 = null;
+        } else {
+
+            FileWriter arq = new FileWriter(file2);
+
+            PrintWriter gravar = new PrintWriter(arq);
+            gravar.printf(telaprincipal.jTextArea.getText());
+
+            gravar.close();
+        }
+    }
+
+    public void novo() {
+        s.setArea(telaprincipal.jTextArea.getText());
+        if (s.area.isEmpty()) {
+            return;
+        }
+
+        int escolha = JOptionPane.showConfirmDialog(null, "Deseja criar novo Texto ? Att : voce perderá tudo que foi "
+                + "\nDigitado até o momento.");
+
+        if (escolha == 0) {
+            telaprincipal.jTextArea.setText("");
+        }
+    }
+
+    public void sair() {
+        s.setArea(telaprincipal.jTextArea.getText());
+        if (s.area.isEmpty()) {
+            System.exit(0);
+
+        } else {
+            int escolha = JOptionPane.showConfirmDialog(null, "Voce esta perto de encerrar o seu programa Att : voce perderá tudo que foi "
+                    + "\nDigitado até o momento.");
+
+            if (escolha == 0) {
+                System.exit(0);
+
+            }
+        }
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if ("sair".equals(e.getActionCommand())) {
+            sair();
+
+        }
+        if ("novo".equals(e.getActionCommand())) {
+
+            novo();
+            file2 = null;
+        }
+
+        if ("abrir".equals(e.getActionCommand())) {
+            try {
+                s.setArea(telaprincipal.jTextArea.getText());
+                if (s.area.isEmpty()) {
+                    Abrir();
+                } else {
+                    int escolha = JOptionPane.showConfirmDialog(null, "Deseja Abrir novo Texto ? Att : Salve para vocé não Perder os seu Dados "
+                            + "\nDigitado até o momento."
+                            + "\nDeseja Continuar");
+
+                    if (escolha == 0) {
+                        telaprincipal.jTextArea.setText("");
+                        Abrir();
+                    }
+                }
+            } catch (IOException ex) {
+
+            }
+        }
+        if ("salvarcomo".equals(e.getActionCommand())) {
+            file2 = null;
+            salvarcomo();
+
+        }
+        if ("salvar".equals(e.getActionCommand())) {
+            try {
+                salvar();
+            } catch (IOException ex) {
+                Logger.getLogger(Eventos.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    }
+}
--- Editor_Texto_Huffman/src/TelaPrincipal.java
+++ Editor_Texto_Huffman/src/TelaPrincipal.java
@@ -13,8 +13,12 @@ public class TelaPrincipal extends javax.swing.JFrame {
     /**
      * Creates new form TelaPrincipal
      */
+    Eventos l = new Eventos(this);
+    Eventos1 l1 = new Eventos1(this);
+
     public TelaPrincipal() {
         initComponents();
+        setTitle("UnescRar");
     }
 
     /**
@@ -27,135 +31,237 @@ public class TelaPrincipal extends javax.swing.JFrame {
     private void initComponents() {
 
         jSeparator1 = new javax.swing.JSeparator();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextPane1 = new javax.swing.JTextPane();
         jPanel1 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jRadioButtonMenuItem1 = new javax.swing.JRadioButtonMenuItem();
-        jRadioButtonMenuItem2 = new javax.swing.JRadioButtonMenuItem();
-        menu_salvar = new javax.swing.JRadioButtonMenuItem();
-        jRadioButtonMenuItem3 = new javax.swing.JRadioButtonMenuItem();
-        jRadioButtonMenuItem4 = new javax.swing.JRadioButtonMenuItem();
-        jRadioButtonMenuItem5 = new javax.swing.JRadioButtonMenuItem();
+        btsalvar = new javax.swing.JButton();
+        btnovo = new javax.swing.JButton();
+        btabrir = new javax.swing.JButton();
+        btcompactar = new javax.swing.JButton();
+        btsalvacomo = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea = new javax.swing.JTextArea();
+        JMenuBarra = new javax.swing.JMenuBar();
+        jMenu = new javax.swing.JMenu();
+        MenuItem_novo = new javax.swing.JRadioButtonMenuItem();
+        MenuItem_abrir = new javax.swing.JRadioButtonMenuItem();
+        MenuItem_salvar = new javax.swing.JRadioButtonMenuItem();
+        MenuItem_salvar_como = new javax.swing.JRadioButtonMenuItem();
+        MenuItem_compactar = new javax.swing.JRadioButtonMenuItem();
+        MenuItem_sair = new javax.swing.JRadioButtonMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Editor de texto Implementando Huffman");
         setResizable(false);
 
-        jScrollPane1.setViewportView(jTextPane1);
-
         jPanel1.setEnabled(false);
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/salvar 50x50.png"))); // NOI18N
+        btsalvar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/salvar 50x50.png"))); // NOI18N
+        btsalvar.addActionListener(l1);
+        btsalvar.setActionCommand("btsalvar");
+        btsalvar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btsalvarActionPerformed(evt);
+            }
+        });
 
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/novo 50x50.png"))); // NOI18N
+        btnovo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/novo 50x50.png"))); // NOI18N
+        btnovo.addActionListener(l1);
+        btnovo.setActionCommand("btnovo");
+        btnovo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnovoActionPerformed(evt);
+            }
+        });
 
-        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/abrir 50x50.png"))); // NOI18N
+        btabrir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/abrir 50x50.png"))); // NOI18N
+        btabrir.addActionListener(l1);
+        btabrir.setActionCommand("btabrir");
+        btabrir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btabrirActionPerformed(evt);
+            }
+        });
 
-        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/compactar 50x50.png"))); // NOI18N
+        btcompactar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/compactar 50x50.png"))); // NOI18N
+        btcompactar.addActionListener(l1);
+        btcompactar.setActionCommand("btcompactar");
+        btcompactar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btcompactarActionPerformed(evt);
+            }
+        });
 
-        jButton5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/salvar como 50x50.png"))); // NOI18N
+        btsalvacomo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/salvar como 50x50.png"))); // NOI18N
+        btsalvacomo.addActionListener(l1);
+        btsalvacomo.setActionCommand("btsalvacomo");
+        btsalvacomo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btsalvacomoActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addComponent(jButton2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton5)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton4)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(btnovo)
+                .addGap(18, 18, 18)
+                .addComponent(btabrir)
+                .addGap(18, 18, 18)
+                .addComponent(btsalvar)
+                .addGap(18, 18, 18)
+                .addComponent(btsalvacomo)
+                .addGap(18, 18, 18)
+                .addComponent(btcompactar)
+                .addContainerGap(176, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jButton2)
-            .addComponent(jButton3)
-            .addComponent(jButton1)
-            .addComponent(jButton4)
-            .addComponent(jButton5)
+            .addComponent(btnovo)
+            .addComponent(btabrir)
+            .addComponent(btsalvar)
+            .addComponent(btcompactar)
+            .addComponent(btsalvacomo)
         );
 
-        jMenu1.setText("Arquivo");
+        jTextArea.setColumns(20);
+        jTextArea.setRows(5);
+        jTextArea.setVerifyInputWhenFocusTarget(false);
+        jScrollPane2.setViewportView(jTextArea);
 
-        jRadioButtonMenuItem1.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_N, java.awt.event.InputEvent.CTRL_MASK));
-        jRadioButtonMenuItem1.setSelected(true);
-        jRadioButtonMenuItem1.setText("Novo");
-        jRadioButtonMenuItem1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/novo 25x25.png"))); // NOI18N
-        jMenu1.add(jRadioButtonMenuItem1);
+        jMenu.setText("Acessando Arquivos ");
 
-        jRadioButtonMenuItem2.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_A, java.awt.event.InputEvent.CTRL_MASK));
-        jRadioButtonMenuItem2.setSelected(true);
-        jRadioButtonMenuItem2.setText("Abrir");
-        jRadioButtonMenuItem2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/abrir 25x25.png"))); // NOI18N
-        jMenu1.add(jRadioButtonMenuItem2);
-
-        menu_salvar.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.CTRL_MASK));
-        menu_salvar.setSelected(true);
-        menu_salvar.setText("Salvar");
-        menu_salvar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/salvar 25x25.png"))); // NOI18N
-        menu_salvar.addActionListener(new java.awt.event.ActionListener() {
+        MenuItem_novo.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_N, java.awt.event.InputEvent.CTRL_MASK));
+        MenuItem_novo.setSelected(true);
+        MenuItem_novo.setText("Novo");
+        MenuItem_novo.addActionListener(l);
+        MenuItem_novo.setActionCommand("novo");
+        MenuItem_novo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/novo 25x25.png"))); // NOI18N
+        MenuItem_novo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                menu_salvarActionPerformed(evt);
+                MenuItem_novoActionPerformed(evt);
             }
         });
-        jMenu1.add(menu_salvar);
+        jMenu.add(MenuItem_novo);
 
-        jRadioButtonMenuItem3.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.SHIFT_MASK | java.awt.event.InputEvent.CTRL_MASK));
-        jRadioButtonMenuItem3.setSelected(true);
-        jRadioButtonMenuItem3.setText("Salvar Como");
-        jRadioButtonMenuItem3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/salvar como 25x25.png"))); // NOI18N
-        jMenu1.add(jRadioButtonMenuItem3);
+        MenuItem_abrir.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_A, java.awt.event.InputEvent.CTRL_MASK));
+        MenuItem_abrir.setSelected(true);
+        MenuItem_abrir.setText("Abrir");
+        MenuItem_abrir.addActionListener(l);
+        MenuItem_abrir.setActionCommand("abrir");
+        MenuItem_abrir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/abrir 25x25.png"))); // NOI18N
+        MenuItem_abrir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MenuItem_abrirActionPerformed(evt);
+            }
+        });
+        jMenu.add(MenuItem_abrir);
 
-        jRadioButtonMenuItem4.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_D, java.awt.event.InputEvent.CTRL_MASK));
-        jRadioButtonMenuItem4.setSelected(true);
-        jRadioButtonMenuItem4.setText("Compactar");
-        jRadioButtonMenuItem4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/compactar 25x25.png"))); // NOI18N
-        jMenu1.add(jRadioButtonMenuItem4);
+        MenuItem_salvar.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.CTRL_MASK));
+        MenuItem_salvar.setSelected(true);
+        MenuItem_salvar.setText("Salvar");
+        MenuItem_salvar.addActionListener(l);
+        MenuItem_salvar.setActionCommand("salvar");
+        MenuItem_salvar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/salvar 25x25.png"))); // NOI18N
+        MenuItem_salvar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MenuItem_salvarActionPerformed(evt);
+            }
+        });
+        jMenu.add(MenuItem_salvar);
 
-        jRadioButtonMenuItem5.setSelected(true);
-        jRadioButtonMenuItem5.setText("Sair");
-        jMenu1.add(jRadioButtonMenuItem5);
+        MenuItem_salvar_como.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.SHIFT_MASK | java.awt.event.InputEvent.CTRL_MASK));
+        MenuItem_salvar_como.setSelected(true);
+        MenuItem_salvar_como.setText("Salvar Como");
+        MenuItem_salvar_como.addActionListener(l);
+        MenuItem_salvar_como.setActionCommand("salvarcomo");
+        MenuItem_salvar_como.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/salvar como 25x25.png"))); // NOI18N
+        MenuItem_salvar_como.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MenuItem_salvar_comoActionPerformed(evt);
+            }
+        });
+        jMenu.add(MenuItem_salvar_como);
 
-        jMenuBar1.add(jMenu1);
+        MenuItem_compactar.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_D, java.awt.event.InputEvent.CTRL_MASK));
+        MenuItem_compactar.setSelected(true);
+        MenuItem_compactar.setText("Compactar");
+        MenuItem_compactar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/compactar 25x25.png"))); // NOI18N
+        jMenu.add(MenuItem_compactar);
 
-        setJMenuBar(jMenuBar1);
+        MenuItem_sair.setSelected(true);
+        MenuItem_sair.setText("Sair");
+        MenuItem_sair.addActionListener(l);
+        MenuItem_sair.setActionCommand("sair");
+        MenuItem_sair.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MenuItem_sairActionPerformed(evt);
+            }
+        });
+        jMenu.add(MenuItem_sair);
+
+        JMenuBarra.add(jMenu);
+
+        setJMenuBar(JMenuBarra);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 663, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jScrollPane2)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 550, Short.MAX_VALUE))
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 557, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         setSize(new java.awt.Dimension(679, 674));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void menu_salvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menu_salvarActionPerformed
-        
-    }//GEN-LAST:event_menu_salvarActionPerformed
+    private void MenuItem_salvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MenuItem_salvarActionPerformed
+
+    }//GEN-LAST:event_MenuItem_salvarActionPerformed
+
+    private void MenuItem_sairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MenuItem_sairActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_MenuItem_sairActionPerformed
+
+    private void MenuItem_novoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MenuItem_novoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_MenuItem_novoActionPerformed
+
+    private void MenuItem_abrirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MenuItem_abrirActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_MenuItem_abrirActionPerformed
+
+    private void MenuItem_salvar_comoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MenuItem_salvar_comoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_MenuItem_salvar_comoActionPerformed
+
+    private void btnovoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnovoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnovoActionPerformed
+
+    private void btabrirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btabrirActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btabrirActionPerformed
+
+    private void btsalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btsalvarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btsalvarActionPerformed
+
+    private void btsalvacomoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btsalvacomoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btsalvacomoActionPerformed
+
+    private void btcompactarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btcompactarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btcompactarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -193,22 +299,26 @@ public class TelaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuBar JMenuBarra;
+    public javax.swing.JRadioButtonMenuItem MenuItem_abrir;
+    public javax.swing.JRadioButtonMenuItem MenuItem_compactar;
+    public javax.swing.JRadioButtonMenuItem MenuItem_novo;
+    public javax.swing.JRadioButtonMenuItem MenuItem_sair;
+    public javax.swing.JRadioButtonMenuItem MenuItem_salvar;
+    private javax.swing.JRadioButtonMenuItem MenuItem_salvar_como;
+    public javax.swing.JButton btabrir;
+    public javax.swing.JButton btcompactar;
+    public javax.swing.JButton btnovo;
+    private javax.swing.JButton btsalvacomo;
+    public javax.swing.JButton btsalvar;
+    private javax.swing.JMenu jMenu;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JRadioButtonMenuItem jRadioButtonMenuItem1;
-    private javax.swing.JRadioButtonMenuItem jRadioButtonMenuItem2;
-    private javax.swing.JRadioButtonMenuItem jRadioButtonMenuItem3;
-    private javax.swing.JRadioButtonMenuItem jRadioButtonMenuItem4;
-    private javax.swing.JRadioButtonMenuItem jRadioButtonMenuItem5;
-    private javax.swing.JScrollPane jScrollPane1;
+    public javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JTextPane jTextPane1;
-    private javax.swing.JRadioButtonMenuItem menu_salvar;
+    public javax.swing.JTextArea jTextArea;
     // End of variables declaration//GEN-END:variables
+
+    String JTextArea(String readLine) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
